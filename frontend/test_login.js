68e89cf643cba36965bbbf147fb540334d537efe
Ruslan <launger69@gmail.com>
2026-03-23 19:23:48 +0700

    main

--- frontend/test_login.js
+++
@@ -1,6 +0,0 @@
-
-fetch('http://localhost:8080/api/v1/auth/login', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ email: 'admin@example.com', password: 'password' })
-}).then(r => r.json()).then(console.log);
